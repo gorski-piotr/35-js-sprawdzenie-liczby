c4e1ef25ea8066b3f98fc138344d346b8ebd76a4
Piotr Gorski <gorskipiotr5@gmail.com>
2021-01-26 14:28:34 +0000

    number check

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function myFunction (n) {
+    if (n >= 100 && n <= 200) {
+        return ("Liczba znajduje się w przedziale");
+    } else {
+        return ("Liczba nie znajduje się w przedziale");
+    }
+}
+
+
+console.log(myFunction(200));
